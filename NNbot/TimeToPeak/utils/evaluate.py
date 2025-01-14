331de16be03ddb1ab908d535592b149cd5e9db08
jgoldberg8 <89888020+jgoldberg8@users.noreply.github.com>
2025-01-13 21:24:54 -0500

    fix

--- NNbot/TimeToPeak/utils/evaluate.py
+++ NNbot/TimeToPeak/utils/evaluate.py
@@ -105,7 +105,7 @@ class RealTimeEvaluator:
             # print(f"Number of features collected at time {current_time}: {len(features_dict)}")
             
             # Need minimum number of features before making prediction
-            if len(features_dict) < 17 * 2:  # 4 granularities
+            # if len(features_dict) < 17 * 2:  # 4 granularities
                 continue
                 
             # Prepare features and make prediction
