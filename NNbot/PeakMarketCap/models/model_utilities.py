b980f7bf2d1094d0a24c7fc8e601b5b51d430a61
jgoldberg8 <89888020+jgoldberg8@users.noreply.github.com>
2025-01-29 19:47:38 -0500

    percent increase at 30

--- NNbot/PeakMarketCap/models/model_utilities.py
+++ NNbot/PeakMarketCap/models/model_utilities.py
@@ -30,7 +30,7 @@ def clean_dataset(df):
         'time_to_peak'
     ]
     df = df.dropna(subset=critical_cols)
-    df = df[df['hit_peak_before_30'] > True]
+    df = df[df['hit_peak_before_30'] == False]
 
     
     
