f101e21179c8f76fd3e5447e407c3dd255c5ba48
jgoldberg8 <89888020+jgoldberg8@users.noreply.github.com>
2025-02-03 17:29:51 -0500

    percent increase at 30

--- NNbot/utils/early_stopping.py
+++ NNbot/utils/early_stopping.py
@@ -1,29 +1,33 @@
 
 class EarlyStopping:
-    def __init__(self, patience=10, min_delta=0):
+    def __init__(self, patience=7, min_delta=0.001, restore_best_weights=True):
         self.patience = patience
         self.min_delta = min_delta
-        self.counter = 0
+        self.restore_best_weights = restore_best_weights
         self.best_loss = None
-        self.early_stop = False
-        
-    def __call__(self, val_loss):
+        self.best_weights = None
+        self.wait = 0
+        self.stopped_epoch = 0
+        self.should_stop = False
+
+    def __call__(self, model, val_loss, epoch):
         if self.best_loss is None:
             self.best_loss = val_loss
-            return False
-        
-        if val_loss > self.best_loss - self.min_delta:
-            self.counter += 1
-            if self.counter >= self.patience:
-                return True
-        else:
+            if self.restore_best_weights:
+                self.best_weights = {k: v.cpu().clone() for k, v in model.state_dict().items()}
+        elif val_loss > self.best_loss - self.min_delta:  # No improvement
+            self.wait += 1
+            if self.wait >= self.patience:
+                self.stopped_epoch = epoch
+                self.should_stop = True
+        else:  # Improvement
             self.best_loss = val_loss
-            self.counter = 0
-            
-        return False
-    
-    def reset(self):
-        """Reset the early stopping state"""
-        self.counter = 0
-        self.best_loss = None
-        self.early_stop = False
+            if self.restore_best_weights:
+                self.best_weights = {k: v.cpu().clone() for k, v in model.state_dict().items()}
+            self.wait = 0
+        
+        return self.should_stop
+
+    def restore(self, model):
+        if self.restore_best_weights and self.best_weights is not None:
+            model.load_state_dict(self.best_weights)
