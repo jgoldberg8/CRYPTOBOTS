f0cf7d561adf81bcccaf4115b2b1d276bba2180e
jgoldberg8 <89888020+jgoldberg8@users.noreply.github.com>
2025-02-03 01:06:43 -0500

    percent increase at 30

--- NNbot/PeakMarketCap/models/peak_market_cap_model.py
+++ NNbot/PeakMarketCap/models/peak_market_cap_model.py
@@ -359,9 +359,10 @@ def train_peak_market_cap_model(train_loader, val_loader,
                     torch.nn.utils.clip_grad_norm_(peak_market_cap_model.parameters(), max_norm=5.0)
                     
                     scaler.step(optimizer)
+                    optimizer.zero_grad(set_to_none=True)
                     scheduler.step()
                     scaler.update()
-                    optimizer.zero_grad(set_to_none=True)
+                    
                     
                     # Update EMA model with momentum scheduling
                     ema.update_parameters(peak_market_cap_model)
@@ -378,8 +379,9 @@ def train_peak_market_cap_model(train_loader, val_loader,
                 if (batch_idx + 1) % accumulation_steps == 0:
                     torch.nn.utils.clip_grad_norm_(peak_market_cap_model.parameters(), max_norm=5.0)
                     optimizer.step()
-                    scheduler.step()
                     optimizer.zero_grad(set_to_none=True)
+                    scheduler.step()
+                    
                     
                     ema.update_parameters(peak_market_cap_model)
             
