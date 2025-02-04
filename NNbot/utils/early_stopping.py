4440062e7473615827eb71feb52bf2a8a401b3a3
jgoldberg8 <89888020+jgoldberg8@users.noreply.github.com>
2025-02-03 20:34:09 -0500

    new peak model

--- NNbot/utils/early_stopping.py
+++ NNbot/utils/early_stopping.py
@@ -1,4 +1,3 @@
-
 class EarlyStopping:
     def __init__(self, patience=7, min_delta=0.001, restore_best_weights=True):
         self.patience = patience
@@ -13,20 +12,18 @@ class EarlyStopping:
     def __call__(self, model, val_loss, epoch):
         if self.best_loss is None:
             self.best_loss = val_loss
-            if self.restore_best_weights:
-                self.best_weights = {k: v.cpu().clone() for k, v in model.state_dict().items()}
-        elif val_loss > self.best_loss - self.min_delta:  # No improvement
+            self.best_weights = {k: v.cpu().clone() for k, v in model.state_dict().items()}
+        elif val_loss > (self.best_loss - self.min_delta):
             self.wait += 1
             if self.wait >= self.patience:
                 self.stopped_epoch = epoch
                 self.should_stop = True
-        else:  # Improvement
+        else:
             self.best_loss = val_loss
-            if self.restore_best_weights:
-                self.best_weights = {k: v.cpu().clone() for k, v in model.state_dict().items()}
+            self.best_weights = {k: v.cpu().clone() for k, v in model.state_dict().items()}
             self.wait = 0
-        
-        return self.should_stop
+            
+        return self.should_stop  # Return the stop flag
 
     def restore(self, model):
         if self.restore_best_weights and self.best_weights is not None:
